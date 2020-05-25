bb1105e845b47f8f4fc5d93d1789b09f4ef67f8d
FilStas <filstas23@gmail.com>
2020-05-25 16:51:14 +0400

    added Conditioner.java and ConditionerTest.java

---
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -0,0 +1,41 @@
+package ru.netology.domain;
+
+public class Conditioner {
+    private String name;
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+    private boolean on;
+
+    public void increaseCurrentTemperature(int currentTemperature){
+        if (currentTemperature > maxTemperature){
+            return;
+        }
+        this.currentTemperature = currentTemperature;
+    }
+
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+        this.currentTemperature = currentTemperature;
+    }
+}
---
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -0,0 +1,20 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Callable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+
+    @Test
+    void increaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        int maxTemperature = 30;
+
+
+    }
+}
+
+
